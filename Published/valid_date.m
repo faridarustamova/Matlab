3db751e43aeb639fd6fb09bfda72949fdc49178a
faridarustamova <80109089+faridarustamova@users.noreply.github.com>
2021-03-05 23:59:24 -0800

    Update valid_date.m

--- Published/valid_date.m
+++ Published/valid_date.m
@@ -1,25 +1,21 @@
 function valid=valid_date(year,month,day)
-  if ~isscalar(year)||~isscalar(month)||~isscalar(day)
+  if ~isscalar(year) || ~isscalar(month) || ~isscalar(day) 
        valid=false;
-   else 
-       valid=true;
+       return
    end
    
    if year < 1 || month < 1 || day < 1
        valid=false;
-   else
-       valid=true;
+   return 
    end
    
    if year ~= fix(year) || month ~= fix(month) || day ~= fix(day)
        valid=false;
-   else
-       valid=true;
+    return
    end
    if month > 12 || day > 31
        valid=false;
-   else
-       valid=true;
+   return
    end
    
    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && day <= 31
@@ -34,11 +30,16 @@ function valid=valid_date(year,month,day)
    
    if (year/4 == fix(year/4)  && year/100 ~= fix(year/100) ) || year/400 == fix(year/400)       
        fprintf('%d is a leap year\n',year);
+       year_type=1;
    else
        fprintf('%d is not a leap year\n',year);
+       year_type=2;
    end
    
-   if ((year/4 == fix(year/4)  && year/100 ~= fix(year/100) ) || year/400 == fix(year/400)) && month==2
-       day<=29;
+   if year_type==1 && month==2 && day<=29
+       valid=true;
+       return
    end
+   
+  
 end
